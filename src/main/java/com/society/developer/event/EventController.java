152646f81c503bb595877be2c8d1a1359881d699
Argie <sd.argie.mula.com>
2022-02-12 23:29:30 +0800

    Initial Commit

---
+++ src/main/java/com/society/developer/event/EventController.java
@@ -0,0 +1,9 @@
+package com.society.developer.event;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EventController {
+
+	
+}
---
+++ src/main/java/com/society/developer/event/EventScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.society.developer.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventScheduleApplication.class, args);
+	}
+
+}
